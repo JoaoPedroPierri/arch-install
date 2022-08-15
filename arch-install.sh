afa663bb6c32332e0dbefd1497d36f6f682939c1
JoaoPedroPierri <joaopedropierri2002@gmail.com>
2022-08-15 10:24:09 -0300

    edit

--- arch-install.sh
+++ arch-install.sh
@@ -8,45 +8,30 @@ format_disk(){
   clear
   echo -e "[!] - Formatando os discos\n"
   sleep 2
-  mkfs.vfat -F32 /dev/sda1
-  mkfs.btrfs -f /dev/sda2
-}
-
-subvolumes(){
-  clear
-  echo -e "[!] - Criando subvolumes em btrfs\n"
-  sleep 2
-  mount /dev/sda2 /mnt
-  btrfs su cr /mnt/@
-  btrfs su cr /mnt/@home
-  btrfs su cr /mnt/@var
-  btrfs su cr /mnt/@snapshots
-  chattr +C /mnt/@var
-  umount /mnt
+  mkfs.vfat -F32 /dev/sda1 
+  mkfs.ext4 /dev/sda2 
+  mkfs.ext4 /dev/sda3
+  mkswap /dev/sda3 
+  mkfs.ext4 /dev/sda4 
 }
 
 mount_partitions(){
   clear
   echo -e "[!] - Montando as partições\n"
   sleep 2
-  mount -o defaults,compress=zstd,nossd,autodefrag,subvol=@ /dev/sda2 /mnt
-  mkdir -p /mnt/boot/efi
-  mkdir /mnt/home
-  mkdir /mnt/var
-  mkdir /mnt/.snapshots
-  mount -o defaults,compress=zstd,nossd,autodefrag,subvol=@home /dev/sda2 /mnt/home
-  mount -o defaults,compress=zstd,nossd,autodefrag,subvol=@var /dev/sda2 /mnt/var
-  mount -o defaults,compress=zstd,nossd,autodefrag,subvol=@snapshots /dev/sda2 /mnt/.snapshots
-  mount /dev/sda1 /mnt/boot/efi
+ mount /dev/sda2 /mnt 
+ mkdir -p /mnt/boot/efi && mount /dev/sda1 /mnt/boot/efi 
+ mkdir /mnt/home && mount /dev/sda4 /mnt/home 
+ swapon /dev/sda3 
 }
 
 pacstrap_arch(){
   clear
   echo -e "[!] - Instalando os pacotes base do Arch Linux\n"
   sleep 2
-  pacstrap /mnt base dhcpcd neovim linux-firmware base-devel
+  pacstrap /mnt base neovim linux-firmware base-devel
   pacman -Sy archlinux-keyring --noconfirm
-  pacstrap /mnt base dhcpcd neovim linux-firmware base-devel
+  pacstrap /mnt base neovim linux-firmware base-devel
 }
 
 fstab_gen(){
